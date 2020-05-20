5c1efbae178cd44abcd0b8065464f059bd81b27b
Rasel Ahmed <raselahmed.lucse@gmail.com>
2020-05-20 23:24:31 +0600

    updated with some features

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
 # password-meter
-Password Meter is inspired by pointing system of Password Entropy, in which the main purpose is to help the end-users to have stronger passwords. (Password entropy is a measurement of how unpredictable a password is.)
+[Password Meter] (https://packagist.org/packages/elubilu/password-meter) is inspired by pointing system of Password Entropy, in which the main purpose is to help the end-users to have stronger passwords. (Password entropy is a measurement of how unpredictable a password is.)
+
+
+## Installation
+
+Use the package manager [composer](https://packagist.org/packages/elubilu/password-meter) to install [Password Meter] (https://packagist.org/packages/elubilu/password-meter).
+
+```bash
+composer require elubilu/password-meter
+
+```
+
+## Usage
+
+It's very easy configure on your application. just follow on below: 
+
+```php
+
+
+$app =  new passwordMeter\passwordMeter();
+$app->password_strength("1111"); // return  message = "Very Weak" , strength = 14  , percentage = 11%
+$app->password_strength("aaaa"); // return  message = "Very Weak" , strength = 19 , percentage = 15%
+$app->password_strength("####"); // return  message = "Very Weak" , strength = 21 , percentage = 17%
+$app->password_strength("banglad"); // return  message = "Weak" , strength = 33  , percentage = 26%
+$app->password_strength("bangla1"); // return  message = "Good" , strength = 37  , percentage = 29%
+$app->password_strength("bangladesh"); // return  message = "Good" , strength = 48  , percentage = 38%
+$app->password_strength("bangla1desh"); // return  message = "Good" , strength = 57 , percentage = 45%
+$app->password_strength("Bangla1desh"); // return  message = "Strong", strength = 66, percentage = 52%
+$app->password_strength("Bangladesh#"); // return  message = "Strong", strength = 71, percentage = 56%
+$app->password_strength("Bangla1desh#"); // return  message = "Strong", strength = 79, percentage = 62%
+$app->password_strength("Hello71*Bangla1desh#"); // return  message = "Very Strong" , strength = 132  , percentage = 100%
+```
+
+There are some default messages,
+```php
+$app =  new passwordMeter\passwordMeter();
+$app->get_messages();
+// Example: default messages of package, 
+Array
+(
+    [VERY_WEAK] => Very Weak
+    [WEAK] => Weak
+    [GOOD] => Good
+    [STRONG] => Strong
+    [VERY_STRONG] => Very Strong
+)
+
+```
+
+There are some default configs,
+```php
+$app =  new passwordMeter\passwordMeter();
+$app->get_configs();
+// Example: default configs of package, 
+Array
+(
+    [SMALL_LETTER] => Array
+        (
+            [min] => 0
+            [max] => 26
+        )
+
+    [CAPITAL_LETTER] => Array
+        (
+            [min] => 0
+            [max] => 26
+        )
+
+    [NUMERIC] => Array
+        (
+            [min] => 0
+            [max] => 255
+        )
+
+    [SPECIAL_CHAR] => Array
+        (
+            [min] => 0
+            [max] => 33
+        )
+
+)
+
+```
+
+
+## Contributing
+Pull requests are welcome. For any changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update the tests as appropriate.
+
+## License
+[MIT](https://github.com/elubilu/password-meter/blob/master/LICENSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main/passwordMeter.php
+++ main/passwordMeter.php
@@ -1,77 +1,90 @@
 <?php
 
   namespace passwordMeter;
-  // specify weights
-  $weights = (object)[
-    SMALL_LETTER => 26,
-    CAPITAL_LETTER => 26,
-    NUMERIC=> 10,
-    SPECIAL_CHAR=> 33
-    ];
-       // specify default messages
-    $messages = (object)[
-        VERY_WEAK => "Very Weak",
-        WEAK => "Weak",
-        GOOD => "Good",
-        STRONG => "Strong",
-        VERY_STRONG => "Very Strong"
-    ];
+  
+    $GLOBALS = array(
+        'weights'=>[
+            'SMALL_LETTER' => 26,
+            'CAPITAL_LETTER' => 26,
+            'NUMERIC'=> 10,
+            'SPECIAL_CHAR'=> 33
+        ],
+        'messages'=>[
+            'VERY_WEAK' => "Very Weak",
+            'WEAK' => "Weak",
+            'GOOD' => "Good",
+            'STRONG' => "Strong",
+            'VERY_STRONG' => "Very Strong"
+        ],
+        'configs'=>[
+            'SMALL_LETTER' => [
 
-    // sepecify default configs
-    $configs =(object)[ 
-        SMALL_LETTER  => [
-            min => 0,
-            max => 26
-        ],
-        CAPITAL_LETTER  => [
-            min => 0,
-            max => 26
-        ],
-        NUMERIC => [
-            min => 0,
-            max => 255
-        ],
-        SPECIAL_CHAR => [
-            min => 0,
-            max => 33
-        ] 
-    ];
+            'min' => 0,
+            'max' => 26 
+            ],
+            'CAPITAL_LETTER' => [
+
+                'min' => 0,
+                'max' => 26 
+            ],
+            'NUMERIC' => [
+
+                'min' => 0,
+                'max' => 255 
+            ],
+            'SPECIAL_CHAR' => [
+
+                'min' => 0,
+                'max' => 33 
+            ],
+        ]
+
+
+    );
 
 
   class passwordMeter {
-
+    
+        protected $glob;
+        public function __construct() {
+            global $GLOBALS;
+            $this->glob =& $GLOBALS;
+        }
+       
+       
         public function get_weights() {
-            return $weights; // get weight 
+            // global $weights;
+            return $this->glob['weights']; // get weight 
         }
-        // config: function(config = {}) {
-        //     // override config if you need
-        //     configs = Object.assign(configs, config);
-        //     return this;
-        // },
+       
         public function get_configs() {
-            return $configs; // get configs
+            return $this->glob['configs']; // get configs
         }
-        // message: function(message = {}) {
-        //     // override messages if you need
-        //     messages = Object.assign(messages, message);
-        //     return this;
-        // },
+       
         public function get_messages() {
-            return $messages; // get messages
+            return $this->glob['messages']; // get messages
         }
     
         public function check_size($size_obj, $size) {
             // check validation size
-            return $size_obj->min <= $size && $size_obj->max >= $size; 
+            return $size_obj['min'] <= $size && $size_obj['max'] >= $size; 
         }
        
         public function password_strength($pass) {
+            // global $weights, $configs, $messages;
+
             $len = strlen($pass);
+            if(!$len){
+                $arr = array('message'=>$messages['VERY_WEAK'], 'strength'=>"Password is required");
+                return $arr;
+            }
+            // return $len;s
             $weight = 0;
             // $this->getRandomQuotes();
             try {
                     // collect weight of password
                     $weight = $this->get_weight($pass);
+                    // return $weight;
             } catch (Exception $e) {
                 // throw error
                 // err(error);
@@ -81,35 +94,39 @@
             $ln2Val = log(2)/log(2.71828); //log to lon transfer fro base log2
             $lnXVal = log(pow($weight, $len))/log(2.71828);
             $strength = ceil($lnXVal/$ln2Val);
+            $percnt = ceil($strength*100/128);
+            $percnt = min(100,$percnt);
+            $percnt = $percnt.'%';
+            $messages = $this->glob['messages'];
             
             if ($strength < 28) {
                 // "very weak" callback with message and value
                 
-                $arr = array('message'=>$messages->VERY_WEAK, 'strength'=>$strength);
+                $arr = array('message'=>$messages['VERY_WEAK'], 'strength'=>$strength, 'percentage'=>$percnt);
                 return $arr;
                 // cb($messages->VERY_WEAK, $strength);
             }
             else if($strength < 36) {
                 // "weak" callback with message and value
-                $arr = array('message'=>$messages->WEAK, 'strength'=>$strength);
+                $arr = array('message'=>$messages['WEAK'], 'strength'=>$strength, 'percentage'=>$percnt);
                 return $arr;
                 // cb($messages->WEAK, $strength);
             }
             else if($strength < 60) {
                 // "good" callback with message and value
-                $arr = array('message'=>$messages->GOOD, 'strength'=>$strength);
+                $arr = array('message'=>$messages['GOOD'], 'strength'=>$strength, 'percentage'=>$percnt);
                 return $arr;
                 // cb($messages->GOOD, $strength);
             }
             else if($strength < 128) {
                 // "strong" callback with message and value
-                $arr = array('message'=>$messages->STRONG, 'strength'=>$strength);
+                $arr = array('message'=>$messages['STRONG'], 'strength'=>$strength, 'percentage'=>$percnt);
                 return $arr;
                 // cb($messages->STRONG, $strength);
             }
             else {
                 // "very strong" callback with message and value
-                $arr = array('message'=>$messages->VERY_STRONG, 'strength'=>$strength);
+                $arr = array('message'=>$messages['VERY_STRONG'], 'strength'=>$strength, 'percentage'=>$percnt);
                 return $arr;
                 // cb($messages->VERY_STRONG, $strength);
             }
@@ -117,47 +134,44 @@
 
         }
         function get_weight($pass){
+            // global $weights, $configs, $messages;
             $weight = 0;
-            $capital_letter = strlen(preg_match_all('/[^A-Z]/', '', $pass));
-            $small_letter = strlen(preg_match_all('/[^a-z]/', '', $pass));
-            $numeric = strlen(preg_match_all('/[^0-9]/', '', $pass));
-            $speicial_char = strlen(preg_match_all('/[!@#$%^&*()]/', '', $pass));
+            $capital_letter = preg_match_all('/[A-Z]/',  $pass);
+            $small_letter = preg_match_all('/[a-z]/',  $pass);
+            $numeric = preg_match_all('/[0-9]/', $pass);
+            $speicial_char = preg_match_all('/[!@#$%^&*()]/', $pass);
+
+            // return $small_letter;
             $valid_size = false;
-
+            $weights = $this->glob['weights'];
+            $configs = $this->glob['configs'];
+            $messages = $this->glob['messages'];
             // checking if capital letter here & validation size proper or not
-            if ($capital_letter && ($valid_size = $this->check_size($configs->CAPITAL_LETTER, $capital_letter))) {
-                $weight += $weights->CAPITAL_LETTER;
-            } else {
-                // throw message
-                if (!$valid_size) throw `Capital letter must be min: {$configs->CAPITAL_LETTER->min} and max: {$configs->CAPITAL_LETTER->max}`;
-            }
-
+            if ($capital_letter && ($valid_size = $this->check_size($configs['CAPITAL_LETTER'], $capital_letter))) {
+                $weight += $weights['CAPITAL_LETTER'];
+            } 
+            
             // checking if small letter here & validation size proper or not
-            if ($small_letter && ($valid_size = $this->check_size($configs->SMALL_LETTER, $small_letter))) {
-                $weight += $weights->SMALL_LETTER;
-            } else {
-                // throw message
-                if (!$valid_size) throw `SMALL letter must be min: {$configs->SMALL_LETTER->min} and max: {$configs->SMALL_LETTER->max}`;
-            }
+           
+            if ($small_letter) {
+                $weight +=  $weights['SMALL_LETTER'];
+                // return Gweights;
+            } 
+            
 
             // checking if numeric value here & validation size proper or not
-            if ($numeric && ($valid_size = $this->check_size($configs->NUMERIC, $numeric))) {
-                $weight += $weights->NUMERIC;
-            } else {
-                // throw message
-                if (!$valid_size) throw new Exception(`NUMERIC value must be min: {$configs->NUMERIC->min} and max: {$configs->NUMERIC->max}`);
-            }
+            if ($numeric && ($valid_size = $this->check_size($configs ['NUMERIC'], $numeric))) {
+                $weight += $weights['NUMERIC'];
+            } 
 
              // checking if special letter here & validation size proper or not
-             if ($speicial_char && ($valid_size = $this->check_size($configs->SPECIAL_CHAR, $speicial_char))) {
-                $weight += $weights->SPECIAL_CHAR;
-            } else {
-                // throw message
-                if (!$valid_size) throw `SMALL letter must be min: {$configs->SPECIAL_CHAR->min} and max: {$configs->SPECIAL_CHAR->max}`;
-            }
+             if ($speicial_char && ($valid_size = $this->check_size($configs['SPECIAL_CHAR'], $speicial_char))) {
+                $weight += $weights['SPECIAL_CHAR'];
+            } 
 
             return $weight; // returing weight after calculate
 
         }
     }
     
+
